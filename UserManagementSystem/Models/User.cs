82e4845fde4adf0ca227604708cb293e238b9264
USER <USER@DESKTOP-U836U6G>
2023-10-24 20:02:47 +0600

    Model Class

---
+++ UserManagementSystem/Models/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace UserManagementSystem.Models
+{
+    public class User
+    {
+        [Key]
+         [Required]
+            public string UserId { get; set; }
+
+           
+            public string EmployeeId { get; set; }
+
+            [Required]
+            public string Name { get; set; }
+
+            [Required]
+            public string Mobile { get; set; }
+
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+
+            [Required]
+            public DateTime DateOfBirth { get; set; }
+
+            [Required]
+            public string Password { get; set; }
+        
+    }
+}
