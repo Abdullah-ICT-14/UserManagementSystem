6561fd218c818f26f047de8d4876614dbf969727
Md. Abdullah <abdullah17015@gmail.com>
2023-10-25 08:40:48 +0600

    Migrations

---
+++ UserManagementSystem/Migrations/202310250237055_userInfo.cs
@@ -0,0 +1,31 @@
+﻿namespace UserManagementSystem.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class userInfo : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        EmployeeId = c.String(nullable: false),
+                        Name = c.String(nullable: false),
+                        Mobile = c.String(nullable: false),
+                        Email = c.String(nullable: false),
+                        DateOfBirth = c.DateTime(nullable: false),
+                        Password = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.UserId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+        }
+    }
+}
---
+++ UserManagementSystem/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace UserManagementSystem.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<UserManagementSystem.Models.UserContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(UserManagementSystem.Models.UserContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
