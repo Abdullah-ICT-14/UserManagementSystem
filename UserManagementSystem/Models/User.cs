bce00ea42ac6731681ba3b4ae5eab6bb19658510
Md. Abdullah <abdullah17015@gmail.com>
2023-10-25 08:33:03 +0600

    Model Class, Context Class and Connection String

---
+++ UserManagementSystem/Models/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace UserManagementSystem.Models
+{
+    public class User
+    {
+        [Key]
+        [Required]
+        public string UserId { get; set; }
+
+        [Required]
+        public string EmployeeId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Mobile { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
